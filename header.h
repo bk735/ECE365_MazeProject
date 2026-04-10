8315d9436b287ca1db5f87d66707fb68bb3bd4a0
aryasgit <rayman3304@gmail.com>
2026-04-11 04:40:49 +0530

    Acclamation of point2d task and changes in the header file for the same

--- header.h
+++ header.h
@@ -1,1 +1,27 @@
-#pragma once
+#ifndef HEADER_H //to prevent already defined error during compiling
+#define HEADER_H
+#include <iostream>
+
+//Author Aryaman Gupta
+//writing header file for point2d.cpp
+class Point2D{
+    private: 
+        int x, y;
+
+    public:
+        Point2D(); //default constructor declaration
+        Point2D(int x, int y); //parameterized declaration
+
+        //getter function for X & Y coords
+        int getX() const; //const to make it read only
+        int getY() const;
+
+        //setter function for X & Y coord
+        void setX(int x); //void type as it will return nothing
+        void setY(int y);
+
+        bool operator==(const &Point2D other) const;
+
+};
+
+#endif
