9ba35f524c642e2746e14d0c5a18ab5a527d1ccf
bk735 <kumarbaibhaw2304@gmail.com>
2026-04-12 18:53:44 +0530

    changed few things in header.h Removed using namespace std as it can lead to duplication

--- header.h
+++ header.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-using namespace std;
+
 
 //Author Aryaman Gupta
 //writing signatures in header file for point2d.cpp
@@ -26,45 +26,51 @@ class Point2D{
 //Signatures for Maze2D
 class Maze2D{
     private:
-        vector<vector<int> > grid; //vector of int type vectors --> 2D grid
+        std::vector<std::vector<int> > grid; //vector of int type vectors --> 2D grid
         int size; // N --> maze size NxN
     public:
         Maze2D(); //default constructor
-        Maze2D(const string& filename); //to load maze from file
-        bool valid_maze(const string& filename); //reads and checks the loaded file, false if any discrepancies
-        void display_maze() const; //for printing grid in the cli
+        Maze2D(const std::string& filename); //to load maze from file
+        bool isValidMaze() const; //reads and checks the loaded file, false if any discrepancies
+        void displayMaze() const;//for printing 
+		void saveMaze(const std::string& filename) const; //to save the maze to a file
         int getSize() const; //returns N
         int getCell(int x, int y) const; // returns value of cell(0,1) whether it is accessible in path traversal or not
-        bool isvalid_cell(int x, int y) const; //valid if cell in NxN matrix bounds and has value 1
+        bool isValidCell(int x, int y) const; //valid if cell in NxN matrix bounds and has value 1
 };
 
 //Signatures for Path1D
 class Path1D{
     private:
-        vector<int> steps; // 1=Up, 2=Right, 3=Down, 4=Left, 0=end
-        bool loaded_path; // flag to prevent multiple file loading
+        std::vector<int> steps; // 1=Up, 2=Right, 3=Down, 4=Left, 0=end
+        
     public:
         Path1D(); //default constructor
-        Path1D(const string& filename); //to load path file
-        bool valid_path(const string& filename); //to check for any discrepancies: only [0,1,2,3,4] steps are allowed AND 0 should only be followed by zeros
-        void display_path() const; //printing number-->direction of the step (eg: 1-->Up)
-        int get_num_steps() const; //total entries in path list
-        int get_step(int index) const; //eg: get_step(0) returns say 1(up)ie. the first value in the loaded path, then get_step(1) for the second value and so on. 
+        Path1D(const std::string& filename); //to load path file
+        bool isValidPath() const; //to check for any discrepancies: only [0,1,2,3,4] steps are allowed AND 0 should only be followed by zeros
+        void displayPath() const; //printing number-->direction of the step (eg: 1-->Up)
+		void savePath(const std::string& filename) const; //to save the path to a file
+        int getNumSteps() const; //total entries in path list
+        int getStep(int index) const; //eg: get_step(0) returns say 1(up)ie. the first value in the loaded path, then get_step(1) for the second value and so on. 
 
 };
 
 //Signatures for PathTraversal
 class PathTraversal{
     private:
-        Maze2D maze; //instance of Maze2D
-        Path1D path; //instance of Path1D
+        Maze2D* maze; //instance of Maze2D
+        Path1D* path; //instance of Path1D
         Point2D point; //instance of Point2D
+
+		std::vector<Point2D> path_points; //to store the points traversed in the path
     public:
         PathTraversal();
-        PathTraversal(const string& mazeFile, const string& pathFile); //loads both maze and path file
-        bool traversal(); /* run valid_maze() → if false, print error
+        PathTraversal(const std::string& mazeFile, const std::string& pathFile); //loads both maze and path file
+        bool traverse(); /* run valid_maze() → if false, print error
                              run valid_path() → if false, print error, stop
                              walk step by step using point, checking as you go */
-        void display_result() const; //prints final success/error message
+        void displayResult() const; //prints final success/error message
+		~PathTraversal(); //destructor to free memory allocated for maze and path
+
 };
 #endif
